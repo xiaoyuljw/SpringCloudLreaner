a59e4730e392a007a975114818001c917de70cc3
xiaoyuljw <373822339@qq.com>
2020-09-21 15:37:27 +0800

    logs

---
+++ src/test/java/com/example/springcloud1/Springcloud1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloud1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcloud1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
